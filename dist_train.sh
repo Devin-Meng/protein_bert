b74d76f49d7211ecb7259730a464f00861d0ea4e
Zhaoxu <zhaoxu.meng.cn@gmail.com>
2021-10-02 02:54:45 +0800

    add content in dist_train.sh

---
+++ dist_train.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ulimit -n 2500
+python /home/Zhaoxu/anaconda3/envs/Pytorch/lib/python3.8/site-packages/torch/distributed/launch.py \
+       --nnode=1 --node_rank=0 --nproc_per_node=8 \
+       src/main.py --local_world_size=8 >output/ep5-addeta/train.out 2>output/ep5-addeta/train.err &
