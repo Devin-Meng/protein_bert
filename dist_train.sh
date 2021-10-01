87c429988a7f5a6c93696cd4e1aba7fdf5637fb0
Zhaoxu <zhaoxu.meng.cn@gmail.com>
2021-10-02 03:23:20 +0800

    debug

--- dist_train.sh
+++ dist_train.sh
@@ -3,4 +3,4 @@
 ulimit -n 2500
 python /home/Zhaoxu/anaconda3/envs/Pytorch/lib/python3.8/site-packages/torch/distributed/launch.py \
        --nnode=1 --node_rank=0 --nproc_per_node=8 \
-       main.py --local_world_size=8 >output/ep5-eta5/train.out 2>output/ep5-eta5/train.err &
+       protein_bert/main.py --local_world_size=8 >output/ep5-eta5/train.out 2>output/ep5-eta5/train.err &
